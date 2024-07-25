88e988e2d659a5a6502dd261a9560fd2164cd913
x_air <unchainedsoul@nate.com>
2024-07-25 20:38:33 +0900

    Initial Commit

---
+++ x_tenpo/x_tenpoApp.swift
@@ -0,0 +1,17 @@
+//
+//  x_tenpoApp.swift
+//  x_tenpo
+//
+//  Created by MIN GI KIM on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct x_tenpoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
