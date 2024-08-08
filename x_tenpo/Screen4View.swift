ef89676a10980bd4a2775df2eb2a57d0187632fb
x_air <unchainedsoul@nate.com>
2024-08-08 23:29:53 +0900

    색상 범위 조정

--- x_tenpo/Screen4View.swift
+++ x_tenpo/Screen4View.swift
@@ -42,7 +42,7 @@ struct Screen4View: View {
     }
     
     func generateRandomColor() {
-        let red = Double.random(in: 0...0.5)
+        let red = Double.random(in: 0.4...0.5)
         let green = Double.random(in: 0...0.5)
         let blue = Double.random(in: 0...0.5)
         color = Color(red: red, green: green, blue: blue)
