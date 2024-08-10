2012c74d400e3e9704cb3e6b9ea5608bec487dbf
x_air <unchainedsoul@nate.com>
2024-08-10 23:29:53 +0900

    타이머 더 비정상적으로 변경

--- x_tenpo/Screen3View.swift
+++ x_tenpo/Screen3View.swift
@@ -67,7 +67,7 @@ struct Screen3View: View {
     
     func resetTimer() {
         timer?.invalidate()
-        timeRemaining = 300
+        timeRemaining = 600
         timerActive = false
     }
 }
