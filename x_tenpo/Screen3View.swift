808e7a1a90605f1c23a77ae027fa65b6990c71f7
x_air <unchainedsoul@nate.com>
2024-08-11 01:27:42 +0900

    1000초 초과 설정 가능토록

--- x_tenpo/Screen3View.swift
+++ x_tenpo/Screen3View.swift
@@ -67,7 +67,7 @@ struct Screen3View: View {
     
     func resetTimer() {
         timer?.invalidate()
-        timeRemaining = 600
+        timeRemaining = 1000
         timerActive = false
     }
 }
