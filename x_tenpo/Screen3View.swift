94412a83a2159ea8b6dde0c3777b8f1874cda552
x_air <unchainedsoul@nate.com>
2024-10-17 21:16:17 +0900

    타이머 설정

--- x_tenpo/Screen3View.swift
+++ x_tenpo/Screen3View.swift
@@ -67,7 +67,7 @@ struct Screen3View: View {
     
     func resetTimer() {
         timer?.invalidate()
-        timeRemaining = 560
+        timeRemaining = 570
         timerActive = false
     }
 }
